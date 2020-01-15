ac746936e56eacb8a475f86cfe4302a1fb5dd938
Reese2596 <reese2596@student.cptc.edu>
2020-01-15 13:36:39 -0800

    Created Student Class

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual Student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Primary/Unique key for students
+        /// Number is generated/Auto increment
+        /// </summary>
+        public int StudentId { get; set; }
+
+        /// <summary>
+        /// First and Last Name of the Student (EX: John Doe)
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Option Date of the Students Birth
+        /// Time is ignored
+        /// </summary>
+        public DateTime? DateofBirth { get; set; }
+    }
+}
