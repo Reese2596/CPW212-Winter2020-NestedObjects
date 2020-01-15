1d64f291ff80b03519a0554bffd46f4feaf0f379
Reese2596 <reese2596@student.cptc.edu>
2020-01-15 14:17:01 -0800

    Added documentation for the Course class

--- NestedObjects/Course.cs
+++ NestedObjects/Course.cs
@@ -6,14 +6,30 @@ using System.Threading.Tasks;
 
 namespace NestedObjects
 {
+    /// <summary>
+    /// Represents a individual course 
+    /// </summary>
     public class Course
     {
+        /// <summary>
+        /// Full Title of the course EX: CPW = Computer Programming
+        /// </summary>
         public string CourseTitle { get; set; }
 
+        /// <summary>
+        /// A short description of class and prereqs
+        /// </summary>
         public string CourseDescription { get; set; }
 
+        /// <summary>
+        /// Primary/Unique Course ID
+        /// Auto Implements 
+        /// </summary>
         public int CourseID { get; set; }
 
+        /// <summary>
+        /// College Credits earned upon completion of the course
+        /// </summary>
         public byte Credits { get; set; }
     }
 }
